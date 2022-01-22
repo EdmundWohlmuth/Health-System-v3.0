675110cda18cb031f46315e20bfa690d1a43ac03
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-01-22 11:08:05 -0400

    Prepared Player.cs

---
+++ Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_System_v3._0
+{
+    class Player
+    {
+        public int maxHealth;
+        public int Health;
+
+        public int maxShield;
+        public int Shield;
+
+        public int maxLives;
+        public int Lives;
+
+        public int damage;
+
+        public Player()
+        {
+            maxHealth = 100;
+            Health = 100;
+
+            maxShield = 100;
+            Shield = 100;
+
+            maxLives = 3;
+            Lives = 3;
+        }
+
+        public void TakeDamge()
+        {
+
+        }
+
+        public void Heal()
+        {
+
+        }
+
+        public void RegenerateShield()
+        {
+
+        }
+
+        public void ShowHUD()
+        {
+
+        }
+
+    }
+}
